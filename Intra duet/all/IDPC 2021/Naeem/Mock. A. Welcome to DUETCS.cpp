86f073284ef1da82093bfb1d52b527387d74d1bd
MD.NAEEM-AL-IMRAN <md.naeem.al.imran@gmail.com>
2021-12-31 11:26:42 +0600

    Solved

---
+++ Intra duet/all/IDPC 2021/Naeem/Mock. A. Welcome to DUETCS.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define Fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define Freed freopen ("0in.txt","r",stdin);
+#define Fout freopen ("0out.txt","w",stdout);
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define pi acos(-1.0)
+#define inf 1e18
+#define Mod 1000000007
+#define limit 1000008
+using namespace std;
+
+
+int  main()
+{
+//    Fast
+//    Freed
+//    Fout
+    ll t,tt=1;
+    cout <<"Welcome to DUETCS\n";
+    return 0;
+}
