001bf7db6d2d6e32b080b4e2b4948135783725a7
MD.NAEEM-AL-IMRAN <md.naeem.al.imran@gmail.com>
2022-06-09 12:21:23 +0600

    Solved

--- CSES/1158 Book Shop.cpp
+++ CSES/1158 Book Shop.cpp
@@ -9,42 +9,58 @@
 #define Mod 1000000007
 #define limit 100002
 using namespace std;
-
-ll n,dp[1002][limit],h[1005],s[1005];
-
-ll rec(ll pos,ll x)
+//cses 1158
+/*
+///0-1 knapsack O(n*W). W=Total weight, p=profit, w=weight.
+ll n,dp[1002][limit],w[1005],p[1005];
+ll rec(ll pos,ll W)
 {
     if(pos==n)  return 0;
-    if(dp[pos][x]!=-1)  return dp[pos][x];
-    if(x>=h[pos])
-        dp[pos][x] = s[pos]+rec(pos+1, x-h[pos]);
-    if(dp[pos][x] < rec(pos+1, x))
-        dp[pos][x] = rec(pos+1, x);
-    return dp[pos][x];
+    if(dp[pos][W]!=-1)  return dp[pos][W];
+    if(W>=w[pos])
+        dp[pos][W] = p[pos]+rec(pos+1, W-w[pos]);
+    ll temp = rec(pos+1, W);
+    if(dp[pos][W] < temp)
+        dp[pos][W] = rec(pos+1, W);
+    return dp[pos][W];
 }
 
 void solve(ll tt)
 {
-    ll i,j,x;
-    scanf("%d %d", &n, &x);
+    ll i,j,W;
+    scanf("%d %d", &n, &W);
     for(i=0; i<n; i++)
-    {
-        scanf("%d", &h[i]);
-    }
+        scanf("%d", &w[i]);
     for(i=0; i<n; i++)
-    {
-        scanf("%d", &s[i]);
-    }
-    for(i=0; i<n; i++)
-    {
-        for(j=0; j<=x; j++)
-            dp[i][j] = -1;
-    }
-    printf("%d\n", rec(0,x));
-
+        scanf("%d", &p[i]);
+    memset(dp, -1, sizeof(dp));
+    printf("%d\n", rec(0,W));
     return ;
 }
+*/
 
+///0-1  knapsack O(n*W) memory optimize. W=Total weight, p=profit, w=weight.
+ll n,dp[limit],w[1005],p[1005];
+void solve(ll tt)
+{
+    ll i,j,W;
+    scanf("%d %d", &n, &W);
+    for(i=0; i<n; i++)
+        scanf("%d", &w[i]);
+    for(i=0; i<n; i++)
+        scanf("%d", &p[i]);
+    memset(dp, 0, sizeof(dp));
+
+    for(i=0; i<n; i++)
+        for(j=W; j>=0; j--)
+        {
+            if(j<w[i])  continue;
+            dp[j] = max(dp[j], p[i]+dp[j-w[i]]);
+        }
+
+    printf("%d\n", dp[W]);
+    return ;
+}
 
 int  main()
 {
