4544ec6720c95ffac8a3e603867776201211a809
MD.NAEEM-AL-IMRAN <md.naeem.al.imran@gmail.com>
2023-04-05 15:28:11 +0600

    Solved

---
+++ UVA/p11480.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+#define Fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define Freed freopen ("0in.txt","r",stdin);
+#define Fout freopen ("0out.txt","w",stdout);
+#define ll long long int
+#define pb push_back
+#define pi acos(-1.0)
+const ll limit = 1e6+5;
+const ll Mod = 1e9+7;
+using namespace std;
+
+
+void Please_AC(ll tt)
+{
+    ll i,j,n,m,k,q;
+    n = tt;
+    ll ans = 0;
+    for(i=1; ; i++)
+    {
+        ll rem = n-3LL-i*3LL;
+        if(rem < 0LL )
+        {
+            cout <<ans<<endl;
+            return;
+        }
+        ans += rem/2LL+1;
+    }
+
+    return ;
+}
+
+
+int  main()
+{
+    Fast
+    //Freed
+    //Fout
+    ll t,tt=1;
+    //cin >> tt;
+    for(t=1; ; t++)
+    {
+        cin >> tt;
+        if(!tt)   return 0;
+        cout <<"Case "<<t<<": ";
+        Please_AC(tt);
+    }
+    return 0;
+}
+
+
